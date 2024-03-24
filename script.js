2e85e484015ac43e13c9bdf9bb23b0f632440f51
Darshan Sidapara <152283893+darshansidapara@users.noreply.github.com>
2024-03-24 15:04:03 +0530

    Main Code Uploaded

---
+++ script.js
@@ -0,0 +1,29 @@
+var editor = CodeMirror.fromTextArea(document.getElementById("editor"), {
+  mode: "text/x-c++src",
+  theme: "oceanic-next",
+  lineNumbers: true,
+  autoCloseBrackets: true,
+});
+var width = window.innerWidth;
+editor.setSize(0.7 * width, "500");
+var option = document.getElementById("inlineFormSelectPref")
+option.addEventListener("change",function(){
+    if(option.value == "Java"){
+        editor.setOption("mode","text/x-java")
+    }
+    else if(option.value == "Python"){
+        editor.setOption("mode","text/x-python")
+    }
+    else if(option.value == "C"){
+        editor.setOption("mode","text/x-csrc")
+    }
+    else if(option.value == "JavaScript"){
+        editor.setOption("mode","text/javascript")
+    }
+    else if(option.value == "PHP"){
+        editor.setOption("mode","text/x-php")
+    }
+    else{
+        editor.setOption("mode","text/x-c++src")
+    }
+})
